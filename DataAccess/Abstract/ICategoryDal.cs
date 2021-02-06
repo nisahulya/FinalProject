8b2311aaadaf4a857dce7a37ab3fa8ab97e68da3
nisahulya <nisahulya94@gmail.com>
2021-02-06 23:59:10 +0300

    6 Şubat Ders Sonu

--- DataAccess/Abstract/ICategoryDal.cs
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -1,11 +1,12 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    interface ICategoryDal : IEntityRepository<Category>
+    public interface ICategoryDal : IEntityRepository<Category>
     {
     }
 }
--- DataAccess/Abstract/IOrderDal.cs
+++ DataAccess/Abstract/IOrderDal.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICustomerDal : IEntityRepository<Customer>
+    public interface IOrderDal : IEntityRepository<Order>
     {
     }
 }
