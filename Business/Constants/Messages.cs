e2f9590f375df7fcac42eb5e60fcbf28deed277d
nisahulya <nisahulya94@gmail.com>
2021-02-11 00:01:51 +0300

    10 Şubat Dersi

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        internal static string MaintenanceTime = "Sistem bakımda";
+        internal static string ProductsListed = "Ürünler Listelendi";
+    }
+}
