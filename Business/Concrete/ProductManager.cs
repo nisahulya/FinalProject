4477ef1b355b8e27742dadde17b7b4a31a10e13b
nisahulya <nisahulya94@gmail.com>
2021-02-13 23:20:39 +0300

    13 Şubat Dersi ilk kısım

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -20,6 +20,7 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+
         public IResult Add(Product product)
         {
             //Business code
@@ -35,10 +36,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour==22)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
 
             return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductsListed);
 
